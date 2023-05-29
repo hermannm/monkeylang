f7ac19ea495920c0997f227c8ff6259457f83074
hermannm <hermann.morkrid@gmail.com>
2023-05-29 21:42:36 +0200

    split lexer and token into separate files

--- src/main/kotlin/Lexer.kt
+++ src/main/kotlin/Lexer.kt
@@ -2,48 +2,6 @@
 
 import kotlin.text.isWhitespace
 
-sealed class Token {
-    // Tokens with data
-    data class Identifier(val name: String) : Token()
-    data class Integer(val value: Int) : Token()
-    data class StringLiteral(val literal: String) : Token()
-
-    // Keywords
-    object Function : Token()
-    object Let : Token()
-    object If : Token()
-    object Else : Token()
-    object Return : Token()
-    object True : Token()
-    object False : Token()
-
-    // Single-character tokens
-    object Assign : Token()
-    object Plus : Token()
-    object Minus : Token()
-    object Slash : Token()
-    object Asterisk : Token()
-    object Bang : Token()
-    object LessThan : Token()
-    object GreaterThan : Token()
-    object Comma : Token()
-    object Semicolon : Token()
-    object Colon : Token()
-    object LeftParen : Token()
-    object RightParen : Token()
-    object LeftBrace : Token()
-    object RightBrace : Token()
-    object LeftBracket : Token()
-    object RightBracket : Token()
-
-    // Double-character tokens
-    object Equals : Token()
-    object NotEquals : Token()
-
-    object EndOfFile : Token()
-    object Illegal : Token()
-}
-
 class Lexer(private val input: String) {
     private var currentCharacter: Char = Char.MIN_VALUE
     private var currentPosition: Int = 0
@@ -172,10 +130,12 @@ class Lexer(private val input: String) {
             }
         }
 
+        val string = input.substring(startPosition, currentPosition)
+
         // Skip closing quote
         readCharacter()
 
-        return input.substring(startPosition, currentPosition)
+        return string
     }
 
     private fun skipWhitespace() {
